c5e96ff0af0cf2858c09e66951cb2236e6bb01e9
JFBC-Wastelander <JFBimCopinski@gmail.com>
2024-07-10 11:06:19 -0300

    feat: lógica de remoção de carros

--- concessionaria.js
+++ concessionaria.js
@@ -1,5 +1,6 @@
 const prompt = require("prompt-sync")()
 const concessionaria = []
+
 function adicionar () {
     let carro = {
         marca: "",
@@ -9,22 +10,33 @@ function adicionar () {
     }
     carro.marca = prompt("Informe a marca do novo carro: ")
     carro.modelo = prompt("Informe o modelo desse novo carro: ")
-    carro.preco = prompt("Informe o preço do carro: ")
-    carro.ano = prompt("Informe o ano de fabricação do carro: ")
+    carro.preco = +prompt("Informe o preço do carro: ")
+    carro.ano = +prompt("Informe o ano de fabricação do carro: ")
     concessionaria.push(carro)
-    console.log(`O carro ${carro.marca} foi adicionado a concessionária`)
+    console.log(`O carro ${carro.marca} ${carro.modelo} foi adicionado a concessionária.`)
 }
 
 function remover () {
-
+    let entradaRemover = prompt("Informe a marca do carro que deseja remover: ")
+    let deletou = false
+    for (let i = 0; i < concessionaria.length; i++) {
+    if (entradaRemover.toLowerCase() == concessionaria[i].marca.toLowerCase()) {
+        concessionaria.splice(i, 1)
+        deletou = true
+        console.log("Carro removido com sucesso.")
+    }
+}
+if (!deletou) {
+    console.log("Não temos essa marca na concessionária.")
+}
 }
 
-function listar () {
+const listar = function () {
+    console.log(concessionaria)
+}
+
+const atualizar = () => {
     
-}
-
-function atualizar () {
-
 }
 
 module.exports = {
