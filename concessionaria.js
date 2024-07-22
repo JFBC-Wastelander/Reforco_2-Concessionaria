9f2ef1f9222a38e821a56830a2ed492c57daaf09
Jean <JFBimCopinski@gmail.com>
2024-07-22 10:02:13 -0300

    feat: Prettier

--- concessionaria.js
+++ concessionaria.js
@@ -1,47 +1,63 @@
-const prompt = require("prompt-sync")()
-const concessionaria = []
+const prompt = require("prompt-sync")();
+const concessionaria = [];
 
-function adicionar () {
-    let carro = {
-        marca: "",
-        modelo: "",
-        preco: "",
-        ano: "",
-    }
-    carro.marca = prompt("Informe a marca do novo carro: ")
-    carro.modelo = prompt("Informe o modelo desse novo carro: ")
-    carro.preco = +prompt("Informe o preço do carro: ")
-    carro.ano = +prompt("Informe o ano de fabricação do carro: ")
-    concessionaria.push(carro)
-    console.log(`O carro ${carro.marca} ${carro.modelo} foi adicionado a concessionária.`)
+function adicionar() {
+  let carro = {
+    marca: "",
+    modelo: "",
+    preco: "",
+    ano: "",
+  };
+  carro.marca = prompt("Informe a marca do novo carro: ");
+  carro.modelo = prompt("Informe o modelo desse novo carro: ");
+  carro.preco = +prompt("Informe o preço do carro: ");
+  carro.ano = +prompt("Informe o ano de fabricação do carro: ");
+  concessionaria.push(carro);
+  console.log(
+    `O carro ${carro.marca} ${carro.modelo} foi adicionado a concessionária.`
+  );
 }
 
-function remover () {
-    let entradaRemover = prompt("Informe a marca do carro que deseja remover: ")
-    let deletou = false
-    for (let i = 0; i < concessionaria.length; i++) {
+function remover() {
+  console.log(concessionaria);
+  let entradaRemover = prompt("Informe a marca do carro que deseja remover: ");
+  let deletou = false;
+  for (let i = 0; i < concessionaria.length; i++) {
     if (entradaRemover.toLowerCase() == concessionaria[i].marca.toLowerCase()) {
-        concessionaria.splice(i, 1)
-        deletou = true
-        console.log("Carro removido com sucesso.")
+      concessionaria.splice(i, 1);
+      deletou = true;
+      console.log("Carro removido com sucesso.");
     }
-}
-if (!deletou) {
-    console.log("Não temos essa marca na concessionária.")
-}
+  }
+  if (!deletou) {
+    console.log("Não temos essa marca na concessionária.");
+  }
 }
 
 const listar = function () {
-    console.log(concessionaria)
-}
+  console.log(concessionaria);
+};
 
 const atualizar = () => {
-    
-}
+  listar();
+  let indice = +prompt("Qual índice deseja atualizar? ");
+  indice = indice - 1;
+  let carro = {
+    marca: "",
+    modelo: "",
+    preco: "",
+    ano: "",
+  };
+  carro.marca = prompt("Informe a marca do novo carro: ");
+  carro.modelo = prompt("Informe o modelo desse novo carro: ");
+  carro.preco = +prompt("Informe o preço do carro: ");
+  carro.ano = +prompt("Informe o ano de fabricação do carro: ");
+  concessionaria[indice] = carro;
+};
 
 module.exports = {
-    adicionar,
-    remover,
-    listar,
-    atualizar,
-}
+  adicionar,
+  remover,
+  listar,
+  atualizar,
+};
--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const prompt = require('prompt-sync')()
-const concessionaria = require('./concessionaria.js')
-while (true){
+const prompt = require("prompt-sync")();
+const concessionaria = require("./concessionaria.js");
+while (true) {
+  console.log("Escolha uma das opções abaixo:");
+  let entrada = prompt(
+    "1- Adicionar carro. 2- Remover carro. 3- Listar carros. 4- Atualizar carro. 5- Sair. "
+  );
 
-console.log("Escolha uma das opções abaixo:")
-let entrada = prompt("1- Adicionar carro. 2- Remover carro. 3- Listar carros. 4- Atualizar carro. 5- Sair. ")
-
-if (entrada == "Adicionar carro".toLowerCase() || entrada == "Adicionarcarro".toLowerCase || entrada == 1) {
-    concessionaria.adicionar()
-
-} else if (entrada == "Remover carro".toLowerCase() || entrada == "Removercarro".toLowerCase() || entrada == 2) {
-    concessionaria.remover()
-
-} else if (entrada == "Listar carro".toLowerCase() || entrada == "Listarcarro".toLowerCase() || entrada == 3) {
-    concessionaria.listar()
-
-} else if (entrada == "Atualizar carro".toLowerCase() || entrada == "Atualizarcarro".toLowerCase() || entrada == 4) {
-    concessionaria.atualizar()
-
-} else if (entrada == "Sair".toLowerCase() || entrada == 5) {
-    console.log("Saindo...")
-    process.exit()
-
-} else {
-    console.log("Informe um valor correto.")
-}
+  if (
+    entrada == "Adicionar carro".toLowerCase() ||
+    entrada == "Adicionarcarro".toLowerCase ||
+    entrada == 1
+  ) {
+    concessionaria.adicionar();
+  } else if (
+    entrada == "Remover carro".toLowerCase() ||
+    entrada == "Removercarro".toLowerCase() ||
+    entrada == 2
+  ) {
+    concessionaria.remover();
+  } else if (
+    entrada == "Listar carro".toLowerCase() ||
+    entrada == "Listarcarro".toLowerCase() ||
+    entrada == 3
+  ) {
+    concessionaria.listar();
+  } else if (
+    entrada == "Atualizar carro".toLowerCase() ||
+    entrada == "Atualizarcarro".toLowerCase() ||
+    entrada == 4
+  ) {
+    concessionaria.atualizar();
+  } else if (entrada == "Sair".toLowerCase() || entrada == 5) {
+    console.log("Saindo...");
+    process.exit();
+  } else {
+    console.log("Informe um valor correto.");
+  }
 }
